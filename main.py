61a8508838e639eec896e1a9659ffbf843d9d5ed
Velvetvibes69 <amayasunil11@gmail.com>
2026-04-26 19:47:55 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#Printing a greeting message
+print("Hello world!")
+
+#Printing my name
+print("My name is Amaya")
+
+#Printing a fun fact about my self
+#print("I love sketcking and volleyball")
+print("I love sketching and volleyball")
+
+#Performing and pprinting a simple calculation
+print("The result of 60 + 7 is:" ,60 + 7)
